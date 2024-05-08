c5dcc78a9b57cf19040d4b047e2e7899ab853c54
golf-sarawut <positwinyu.sarawut@moneyforward.co.jp>
2024-05-08 18:01:31 +0900

    Add console1 as frontend challenge.

---
+++ app/console1/page.tsx
@@ -0,0 +1,10 @@
+export default function Console1() {
+
+  console.log("console 1");
+
+  return (
+    <main>
+        <h1>Console 1</h1>
+    </main>
+  );
+}
---
+++ app/console1/readme.md
@@ -0,0 +1,5 @@
+# Question
+
+Why isn't console.log display here
+
+![alt text](image.png)
