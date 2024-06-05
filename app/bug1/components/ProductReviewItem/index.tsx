6423e2917b91f26ff30798cba4ade6c8e15bfd44
golf-sarawut <positwinyu.sarawut@moneyforward.co.jp>
2024-06-05 21:34:15 +0900

    refactor.

--- app/bug1/components/ProductReviewItem/index.tsx
+++ app/bug1/components/ProductReviewItem/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Product } from '../../domain/Product';
-import { ProductRating } from '../ProductRating';
+import React from "react";
+import { Product } from "../../domain/Product";
+import { ProductRating } from "../ProductRating";
 
 type ProductReviewItemProps = {
-    product: Product;
-    onRemove: (productId: number) => void;
-    onRate: (productId: number, rate: string) => void;
-}
+  product: Product;
+  onRemove: (productId: number) => void;
+  onRate: (productId: number, rate: string) => void;
+};
 
-export const ProductReviewItem = ({ product, onRemove, onRate }: ProductReviewItemProps) => {
+export const ProductReviewItem = ({
+  product,
+  onRemove,
+  onRate,
+}: ProductReviewItemProps) => {
   return (
     <li className="flex w-fit items-center justify-between bg-white p-4 rounded shadow">
       <div className="flex w-fit items-center">
         <span className="font-medium text-black">{product.name}</span>
-        <ProductRating onRate={(rating)=> onRate(product.id, rating)}/>
+        <ProductRating onRate={(rating) => onRate(product.id, rating)} />
       </div>
       <button
         onClick={() => onRemove(product.id)}
