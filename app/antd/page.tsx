e0473c2fed37069ebbf21368580ebd62632ca916
golf-sarawut <positwinyu.sarawut@moneyforward.co.jp>
2024-06-17 11:47:00 +0900

    add example.

---
+++ app/antd/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Tooltip } from "antd";
+
+export default function Page() {
+    return (
+      <main>
+          <Tooltip title="hello">
+            Test
+          </Tooltip>
+      </main>
+    );
+  }
